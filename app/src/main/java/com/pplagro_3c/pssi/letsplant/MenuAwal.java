b28a5586a69028e6a54067e03acfe669d00aaac2
Insan23 <green.blaze235@gmail.com>
2017-09-24 23:38:38 +0700

    initial Commit

---
+++ app/src/main/java/com/pplagro_3c/pssi/letsplant/MenuAwal.java
@@ -0,0 +1,13 @@
+package com.pplagro_3c.pssi.letsplant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuAwal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_awal);
+    }
+}
